3e090184064aa187c89cf1c3e888331ea45ea0d4
Yogesh Gupta <yogeshgupta984@gmail.com>
2020-06-05 19:02:19 +0530

    Created RandomPickWithWeight

---
+++ java/data-structure/RandomPickWithWeight.java
@@ -0,0 +1,29 @@
+/**
+Check explanation here - https://leetcode.com/problems/random-pick-with-weight/discuss/671460/Detailed-Explanation-or-Summary-of-2-different-solutions
+*/
+class Solution {
+
+    int[] w_sum;
+    public Solution(int[] w) {
+        w_sum = new int[w.length];
+        int sum = 0;
+        for(int i = 0; i < w.length; i++){
+            sum += w[i];
+            w_sum[i] = sum;
+        }
+    }
+    
+    public int pickIndex() {
+        double target = w_sum[w_sum.length - 1] * Math.random();
+        for(int i = 0; i < w_sum.length; i++){
+            if(target < w_sum[i]) return i;
+        }
+        return -1;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
